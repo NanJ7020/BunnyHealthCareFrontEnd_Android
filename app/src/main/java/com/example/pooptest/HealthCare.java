013d73b860d33f376d2ca0b8b9dfdf3de8d66ae3
Nan <nan@Nans-MacBook-Pro.local>
2020-02-17 23:20:34 -0800

    V_02.17

---
+++ app/src/main/java/com/example/pooptest/HealthCare.java
@@ -0,0 +1,23 @@
+package com.example.pooptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HealthCare extends AppCompatActivity {
+    public void PpTest(View view) {
+        Intent intent = new Intent(this,PoopTest.class );
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_care);
+    }
+}
---
+++ app/src/main/java/com/example/pooptest/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.pooptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void login(View view) {
+
+        EditText BunnyNameEditText = findViewById(R.id.BunnyNameEditText);
+
+        EditText BirthDateEditText = findViewById(R.id.OwnerBirthEditText);
+
+        //invalid text
+        String strBunnyName = BunnyNameEditText.getText().toString();
+        String strBirthDate = BirthDateEditText.getText().toString();
+
+        if(TextUtils.isEmpty(strBunnyName)) {
+            //Log.i("hahahahah","Test");
+            BunnyNameEditText.setError("cannot be empty");
+            return;
+        }
+
+        if(TextUtils.isEmpty(strBirthDate)) {
+           BirthDateEditText.setError("cannot be empty");
+           return;
+        }
+
+    }
+
+    public void register(View view) {
+
+        EditText BunnyNameEditText = findViewById(R.id.BunnyNameEditText);
+
+        EditText BirthDateEditText = findViewById(R.id.OwnerBirthEditText);
+
+        //invalid text
+        String strBunnyName = BunnyNameEditText.getText().toString();
+        String strBirthDate = BirthDateEditText.getText().toString();
+
+        if(TextUtils.isEmpty(strBunnyName)) {
+            BunnyNameEditText.setError("cannot be empty");
+            return;
+        }
+
+        if(TextUtils.isEmpty(strBirthDate)) {
+            BirthDateEditText.setError("cannot be empty");
+            return;
+        }
+
+    }
+
+    private Button button;
+
+
+
+                      @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.GuestButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                guest(view);
+            }
+
+            });
+    }
+
+    public void guest(View view) {
+        Intent intent = new Intent(this,HealthCare.class);
+        startActivity(intent);
+
+    }
+}
